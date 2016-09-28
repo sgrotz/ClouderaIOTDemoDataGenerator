05c7ac2f9daacc6ad51cb50b73e441d7e8d3cd71
sgrotz <stephan.grotz@gmail.com>
2016-09-28 10:53:03 +0200

    Changed Logging

--- startDataGenerator.sh
+++ startDataGenerator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "*** Starting DataGenerator ***"
-java -server -cp "target/*:target/libs/*:target/classes/*" -Xms1g -Xmx1g org.cloudera.demo.DataGenerator $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -server -cp "target/*:target/libs/*:target/classes/*" -Dlog4j.configuration=target/classes/log4j.properties -Xms2g -Xmx2g org.cloudera.demo.DataGenerator $1 $2 $3 $4 $5 $6 $7 $8 $9
